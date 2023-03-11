6808cd33a22cb66222b61a489fec5b3eadabac72
micke <micke.simolin@gmail.com>
2023-03-11 16:57:47 +0200

    added a function and a remap for quick console.log() text

--- lua/micke/remap.lua
+++ lua/micke/remap.lua
@@ -1,3 +1,12 @@
+--defining insertConsoleLog func 
+function insertConsoleLog()
+    --inserts str 'console.log()' at current cursor pos.
+    vim.api.nvim_feedkeys("console.log()", "n", true)
+    --move the cursor back one to get inside the parantheses
+    local termcode = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
+    vim.api.nvim_feedkeys(termcode, "n", true)
+end
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<ESC>")
@@ -5,3 +14,5 @@ vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("i", "<leader>log", "<ESC>:lua insertConsoleLog()<CR>a", {noremap = true, silent = true})
