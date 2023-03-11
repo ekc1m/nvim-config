a1419e85c305a628ba2f38e8c3ae1ae56ee6f058
micke <micke.simolin@gmail.com>
2023-03-11 17:02:52 +0200

    increased verbosity in remap.lua

--- lua/micke/remap.lua
+++ lua/micke/remap.lua
@@ -7,12 +7,15 @@ function insertConsoleLog()
     vim.api.nvim_feedkeys(termcode, "n", true)
 end
 
+--defining leader as " " (space) and "jk" as escape in insert mode
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<ESC>")
 
+--defining <leader>y to yank text to sys clipboard instead of current buffer in normal and visual mode
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+--defining <leader>log to execute func insertConsoleLog()
 vim.keymap.set("i", "<leader>log", "<ESC>:lua insertConsoleLog()<CR>a", {noremap = true, silent = true})
